b7b1033407a3e592c2093ae0bf73a3248a585630
Winifred Lu <winnie.lu@comcast.net>
2022-02-07 14:42:32 -0500

    post new user endpoint complete

--- api/server.js
+++ api/server.js
@@ -16,4 +16,22 @@ server.get('/api/users', (req, res) => {
         })
 });
 
+// POST a new user
+server.post('/api/users', (req, res) => {
+    let user = req.body;
+    if(!user.name){
+        res.status(500).json({message: 'name is required'});
+    } else if(!user.bio){
+        res.status(500).json({message: 'bio is required'});
+    } else {
+        userModel.insert(user)
+            .then(user => {
+                res.status(201).json(user);
+            })
+            .catch(() => {
+                res.status(500).json({message: 'could not create user!'});
+            })
+    }
+});
+
 module.exports = server; // EXPORT YOUR SERVER instead of {}
